87d9ea9c364944492ddb146c7efdda2e5caa1d42
Yacov Rosenberg <yacov94@yahoo.com>
2018-01-12 17:33:22 -0200

    category domain class added

--- src/main/java/com/yacov/cursomc/resources/CategoryResource.java
+++ src/main/java/com/yacov/cursomc/resources/CategoryResource.java
@@ -1,17 +1,29 @@
 package com.yacov.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yacov.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "Rest is working";
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
